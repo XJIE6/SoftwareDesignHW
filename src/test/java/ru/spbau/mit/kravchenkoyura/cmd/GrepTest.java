e38ca890c8b1820b3c6ef2d0b41a6f56267a4a1c
XJIE6 <krav4enkoyura@yandex.ru>
2016-12-31 15:34:24 +0300

    workspace removed

---
+++ src/test/java/ru/spbau/mit/kravchenkoyura/cmd/GrepTest.java
@@ -0,0 +1,16 @@
+package ru.spbau.mit.kravchenkoyura.cmd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by YuryKravchenko on 31/12/2016.
+ */
+public class GrepTest {
+
+    @Test
+    public void testEval() throws Exception {
+
+    }
+}
